2d622fb1a842d2600dbfce7a699ce8eb236413c7
tasosa156 <tasosa156@hotmail.com>
2025-05-31 21:35:36 +0200

    Update strecke.php

--- strecke.php
+++ strecke.php
@@ -35,7 +35,7 @@ foreach ($strecken as $index => $s) {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><?= $id ?>. <?= $strecke['name'] ?></title>
-  <link rel="stylesheet" href="/style.css" />
+  <link rel="stylesheet" href="assets/style.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
   <script>
     function toggleMenu() {
